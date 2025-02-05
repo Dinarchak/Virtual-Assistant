4db40d77722e64be49e6537010de28d313f0fded
Dinarchak <jjustdinar@gmail.com>
2025-02-05 21:32:26 +0300

    Вынес принты вне фукнций выполнения, остались принты только в delete_app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import click
 from start_tracking import start_tracking as start_tracking_foo
 from add_app import add_app as add_app_foo
+from delete_app import delete_app as delete_app_foo
+from show_apps import show_apps as show_apps_foo
 
 @click.group()
 def cli():
@@ -11,6 +13,7 @@ def cli():
 @click.option('-n', '--name', help='Имя программы', required=True)
 def add_app(type, name):
     add_app_foo(name, type)
+    click.echo(f"Программа '{name}' добавлена в список отслеживаемых")
 
 @cli.command()
 @click.argument('name', nargs=-1, required=True)
@@ -28,7 +31,7 @@ def show_apps():
 @cli.command()
 def start_tracking():
     click.echo('Отслживаю работу процессов...')
-    start_tracking()
+    start_tracking_foo()
 
 @cli.command()
 def show_statistic():
